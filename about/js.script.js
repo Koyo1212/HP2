f64e872d854e74640db2fc59d163ff66859b197b
taro.yamamoto <koyo36987@icloud.com>
2022-09-20 20:25:11 +0900

    HP

---
+++ about/js.script.js
@@ -0,0 +1,15 @@
+$(function () {
+  $('.hamburger').click(function () {
+    $(this).toggleClass('active');
+
+    if ($(this).hasClass('active')) {
+      $('.globalMenuSp').addClass('active');
+    } else {
+      $('.globalMenuSp').removeClass('active');
+    }
+  });
+});
+
+$('#manu a[href]').on('click', function (event) {
+  $('.navToggle').trigger('click');
+});
